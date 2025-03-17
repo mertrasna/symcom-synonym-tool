0c6ad5e54f33a1b9074eef4bde6889a5a46b41ce
Chelsey <chelseyyfern@gmail.com>
2025-03-17 14:25:23 +0100

    highlight + notsure unchecking working

--- synonym-tool/assets/js/hover.js
+++ synonym-tool/assets/js/hover.js
@@ -55,7 +55,7 @@ $(document).ready(function () {
                 dataType: "json",
                 success: function (response) {
                     if (response.success && response.synonyms.length > 0) {
-                        let popupContent = "<strong></strong><br><br>";
+                        let popupContent = "<strong></strong>";
 
                         response.synonyms.forEach(info => {
                             popupContent += `
